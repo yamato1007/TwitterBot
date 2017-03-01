8992968d108e066ea7cbe43d27b0ddc3fa36ee63
yamato_tanaka <a.niger.333141592653589793@gmail.com>
2017-03-02 04:38:50 +0900

    定刻ツイートとリプライの機能追加

--- Util.hs
+++ Util.hs
@@ -3,6 +3,9 @@ module Util where
 import System.Random
 import qualified Data.ByteString as B
 import Codec.Binary.UTF8.String as Codec
+import Data.Time
+import Text.Regex.Posix
+import qualified Codec.Binary.UTF8.String as U
 
 randChoice :: [a] -> IO a
 randChoice l = do
@@ -16,3 +19,31 @@ strToB s = B.pack $ Codec.encode s
 bToStr :: B.ByteString -> String
 bToStr b = Codec.decode $ B.unpack b
 
+getTimeFormat :: String -> IO String
+getTimeFormat f = fmap (formatTime defaultTimeLocale f ) getZonedTime
+
+separate :: Char -> String -> [String]
+separate c s = foldr check [[]] s
+  where
+    check c' (s:ss) = if c' == c then []:s:ss else (c':s):ss
+
+lookupRegex :: [(String,String)] -> String -> [String]
+lookupRegex m s = foldr match [] m 
+  where 
+    match (k,v) l = if U.encodeString s =~ (U.encodeString k :: String) then v:l else l
+
+mapFromFile :: String -> IO [(String,String)]
+mapFromFile file = do
+  ls <- readFileLn file
+  return $ fmap (\(a:b:_) -> (a,b)) $ fmap (separate ',') ls
+
+readFileLn :: String -> IO [String]
+readFileLn file = fmap lines $ readFile file
+
+headMaybe :: [a] -> Maybe a
+headMaybe [] = Nothing
+headMaybe (a:_) = Just a
+
+headEither :: [a] -> Either String a
+headEither [] = Left "List is Empty"
+headEither (a:_) = Right a
--- main.hs
+++ main.hs
@@ -1,27 +1,58 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-{-# LANGUAGE DeriveGeneric #-}
-
 import Operations
 import Util
+import Settings
+import qualified API as API
 
-tweetUsualFile = "tweetUsual.txt"
+tweetWithPrint :: String -> IO ()
+tweetWithPrint t = do
+  postTweet t >>= dustBox
+  putStrLn $ "Tweeted : " ++ t
 
 tweetUsual :: IO ()
 tweetUsual = do
-  tweets <- fmap lines $ readFile tweetUsualFile
+  tweets <- readFileLn tweetUsualFile
   tweet <- randChoice tweets 
-  postTweet tweet :: TResponse ()
-  putStrLn $ "Tweeted : " ++ tweet
-  return ()
+  tweetWithPrint tweet
 
-printTweets :: String -> Int -> IO ()
-printTweets usr n = do
-  ets <- getTweets usr :: TResponse [Tweet] 
-  let etsn = fmap (take n) ets 
-  (mapM_ . mapM_) (putStrLn.text) etsn
+printTweets :: String -> IO ()
+printTweets usr = do
+  ets <- getTweets usr :: TResponse [API.Tweet] 
+  (mapM_ . mapM_) (putStrLn.API.text) ets
+
+printMentions :: IO ()
+printMentions = do
+  ets <- getMentions :: TResponse [API.Tweet] 
+  (mapM_ . mapM_) (putStrLn.API.text) ets
+
+tweetAtTime :: IO ()
+tweetAtTime = do 
+  time <- getTimeFormat "%H:%I"
+  tm <- mapFromFile tweetAtTimeFile
+  mapM_ tweetWithPrint $ lookup time tm 
+
+reply :: IO ()
+reply = do
+  latest <- readFile latestMentionFile
+  mentions <- twitterRequest API.mentions_timeline [("since_id",latest)] :: TResponse [API.Tweet]
+  replyMap <- mapFromFile replyFile
+  mapM_ (replyToTweets replyMap) mentions
+  mapM_ (writeFile latestMentionFile) $ fmap API.id_str (mentions >>= headEither)
+
+replyToTweets :: [(String,String)] -> [API.Tweet] -> IO ()
+replyToTweets replyMap mentions = mapM_ (replyToTweet replyMap) mentions
+
+replyToTweet :: [(String,String)] -> API.Tweet -> IO ()
+replyToTweet replyMap mention = do
+  let (id,user) = (API.id_str mention, API.screen_name $ API.user mention)
+  let text = head $ (lookupRegex replyMap) (API.text mention)
+  let status = '@':user ++ ' ':text
+  twitterRequest' API.update [("in_reply_to_status_id",id),("status",status)]
+  putStrLn $ "Tweeted : " ++ status
 
 main :: IO ()
 main = do
+  tweetAtTime
   tweetUsual
-
+  reply
+  printTweets "_momoco__"
+  printMentions
